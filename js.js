37657d9b38da31a92ebc15d5f12413ab33e6b4e2
DrBryanMan <btdrbryan@gmail.com>
2023-09-20 23:51:19 +0300

    fix css

--- js.js
+++ js.js
@@ -2,3 +2,12 @@ btnSidebar.onclick = () => {
     sidebar.classList.toggle('is-open')
     console.log("KK")
 }
+
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('header');
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
