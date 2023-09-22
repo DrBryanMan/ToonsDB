f483675fd1990bcd5a2f7a9775a6351d3f367e1a
DrBryanMan <btdrbryan@gmail.com>
2023-09-22 16:58:44 +0300

    search

--- js.js
+++ js.js
@@ -1,13 +1,27 @@
+authWrapper.onclick = () => {
+  loginFormBox.style.display = 'none'
+}
+signInBtn.onclick = () => {
+  loginFormBox.style.display = 'flex'
+}
+
+authSignUpBtn.onclick = () => {
+  loginFormBox.classList.add('active')
+}
+authSignInBtn.onclick = () => {
+  loginFormBox.classList.remove('active')
+}
+
 btnSidebar.onclick = () => {
-    sidebar.classList.toggle('is-open')
-    console.log("KK")
+  sidebar.classList.toggle('is-open')
+  console.log("KK")
 }
 
 window.addEventListener('scroll', function() {
-    let header = document.querySelector('header');
-    if (window.scrollY > 0) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
+  let header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
